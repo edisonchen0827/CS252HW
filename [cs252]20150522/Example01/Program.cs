379472d839c85d73c21a87fa5c664cd2c0ad4cf0
edisonchen0827 <beautyanzi@gmail.com>
2015-05-26 21:38:29 +0800

    HW
    CS252

---
+++ [cs252]20150522/Example01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student[] students = new Student[5];
+            students[0] = new Student(90, 80);
+            students[1] = new Student(24, 90);
+            students[2] = new Student(100, 50);
+            students[3] = new Student(50, 90);
+            students[4] = new Student(30, 80);
+
+            Console.Write("請輸入排序順序，小到大選1，大到小選2:");
+            int OrderOption = int.Parse(Console.ReadLine()); 
+        
+  //          for (int index = 0; index < vectors.Length; index++)
+            Student.Order = OrderOption == 1 ? 1:-1;
+
+            Console.WriteLine("排序前");
+            for (int index = 0; index < students.Length; index++)
+                Console.WriteLine(students[index]);
+            Array.Sort(students);
+
+            Console.WriteLine("排序後");
+            for (int index = 0; index < students.Length ; index++)
+                Console.WriteLine(students[index]);
+
+        }
+    }
+}
---
+++ [cs252]20150522/Example01/Vector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+
+{
+    internal class Student: IComparable
+    {
+        public int 國文 {get;set;}
+        public int 英文 {get;set;}
+        public static int Order { get; set; }
+
+        public int 總分{
+
+            get {
+                return this.國文 + this.英文;           
+            
+            }
+        }
+        public Student(int 國文,int 英文)
+        {
+             this.國文 = 國文;
+             this.英文 = 英文;
+        
+        }
+
+        public int CompareTo(object obj) 
+        {
+            Student  B = obj as Student;
+            if (this.國文 > B.國文)
+                return 1*Order;
+            else if (this.國文 == B.國文)
+                return 0;            
+        　　else　return -1*Order;
+        }
+        public override string ToString() 
+        {
+            string s = string.Format("國文={0},英文={1},總分{2}",this.國文,this.英文,this.總分);
+            return s;
+        
+        }
+
+        
+        
+    }
+}
